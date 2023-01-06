ad92b1c502c0c731ba50eb868f47e895d9469a5f
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-01-06 22:21:39 +0545

    basics

---
+++ LibrariesBasics.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib as plot
+# np.array([1,2,3])
+# print(np.arange(4,dtype=np.float32))
+print(np.linspace(0,10,5))  #create an array with values that are spaced linearly in a specified interval
+print(np.linspace(0,10,5,dtype=np.int64))   #Specifying the data type
