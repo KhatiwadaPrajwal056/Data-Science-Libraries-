b670c78a759873422d19947c3d42fcd7a1ce50fc
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-06-17 09:17:03 +0545

    Practice

--- LibrariesBasics.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib as plot
-np.array([[1,2,3],[2.3,4,5],[6,7,8]])
-# print(np.arange(4,dtype=np.float32))
-print(np.empty(1))
-# a=np.empty(2)
-# print(a)
-# print(np.linspace(0,10,5))  #create an array with values that are spaced linearly in a specified interval
-# print( =
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ML
- 
+Data science Libraries
